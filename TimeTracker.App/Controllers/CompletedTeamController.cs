10c31ff05ca5cc37944b029448acb56c465b1fb7
Josh Hill <joshuahill84@gmail.com>
2016-01-12 13:48:52 -0600

    added complete team m,v,c

---
+++ TimeTracker.App/Controllers/CompletedTeamController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TimeTracker.App.Controllers
+{
+    public class CompletedTeamController : Controller
+    {
+        // GET: CompletedTeam
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
